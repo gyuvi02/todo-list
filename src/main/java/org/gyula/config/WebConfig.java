399826d76e934af96416581e211981bfe9eb2bce
gyuvi02 <Gyulus99>
2021-10-17 23:57:06 +0200

    Mapping started

---
+++ src/main/java/org/gyula/config/WebConfig.java
@@ -0,0 +1,11 @@
+package org.gyula.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "org.gyula")
+public class WebConfig {
+}

646eae4d78ac49cf833a78aaf183e59f9886ab03
gyuvi02 <gyulaother@gmail.com>
2021-10-19 10:34:47 +0200

    Test commit for another user

--- src/main/java/org/gyula/service/DemoServiceImpl.java
+++ src/main/java/org/gyula/service/DemoServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class DemoServiceImpl implements DemoService {
 
+    // publlic methods
     @Override
     public String getHelloMessage(String user) {
         return "Hello, " + user;
